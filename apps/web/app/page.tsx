1b4d8aa3cfa99dc337d0484d17483ee7e587e51d
DoggieK <doggiek007@gmail.com>
2026-06-20 03:58:04 +0800

    refactor: simplify landing page by removing Why Mandate founder story section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
       <SiteHeader />
       <main>
         <Hero />
-        <WhyMandate />
+        {/* <WhyMandate /> */}
         <Problem />
         <HowItWorks />
         <ProductDemo />
